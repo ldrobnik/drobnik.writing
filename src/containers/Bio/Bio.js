bb474d512730c4d30a3a26ddcdb702a4fd4fffe7
Łukasz Drobnik <drobniklukasz@gmail.com>
2019-05-05 20:58:41 +0200

    crate remaining components

---
+++ src/containers/Bio/Bio.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import classes from './Bio.css';
+
+class Bio extends Component {
+    render() {
+        return(
+            <div>Bio</div>
+        );
+    }
+}
+
+export default Bio;
---
+++ src/containers/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import classes from './NavBar.css';
+
+class NavBar extends Component {
+    render() {
+        return(
+            <div>NavBar</div>
+        );
+    }
+}
+
+export default NavBar;
