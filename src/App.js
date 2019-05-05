63934ec30a8c56c2575fb56668b604d41c6e5ed8
Łukasz Drobnik <drobniklukasz@gmail.com>
2019-05-05 20:06:20 +0200

    wrap content in the Layout component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import './App.css';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
-    <div >
-
-    </div>
+      <Layout>
+        Content
+      </Layout>
   );
 }
 
