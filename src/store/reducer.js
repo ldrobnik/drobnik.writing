019d9d32b2154c6ef3838615bc8818b50a3e5041
Łukasz Drobnik <drobniklukasz@gmail.com>
2019-07-08 17:09:15 +0200

    create actions and reducer files

---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+import * as actionTypes from './actions';
+
+const initialState = {
+    language: 'en',
+    theme: 'nocturine'
+};
+
+const reducer = (state = initialState, action) => {
+
+};
+
+export default reducer;
