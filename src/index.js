04049837b033cb1d52c4dbf0da5aa0b4f698abd1
Łukasz Drobnik <drobniklukasz@gmail.com>
2019-07-08 18:49:25 +0200

    set up redux store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
 
 const app = (
-  <Provider>
+  <Provider store={store}>
       <App />
   </Provider>
 );
