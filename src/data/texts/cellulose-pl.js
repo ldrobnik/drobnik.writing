9cc08a8cf15dc99c64e5f0116433299816ce034b
Łukasz Drobnik <drobniklukasz@gmail.com>
2019-07-08 16:31:48 +0200

    add treasures-en text

--- src/data/texts/cellulose-pl.js
+++ src/data/texts/cellulose-pl.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
-export const CELLULOSE_PL = <div className="cunninghamella">
+export const CELLULOSE_PL = <div>
+    <p><span className="cunninghamella">D</span>ni są dla niej sekundami. Aparaty szparkowe mrugają, gdy oddycha. Wchodzi do środka przez wybite okno, gdzie witają ją zatęchły zapach nieruchomego powietrza oraz stary dywan puchaty od pleśni — dom dla roztoczy i czubajek.</p>
+    <p>Nie ma już słów na jej sprężyste pędy i wytrwałe korzenie. Żaden z jej licznych wąsów czepnych nie może być nazwany. Kiedy cicho eksploduje pośrodku salonu, a dni i noce migają jak stroboskop, jej chloroplasty są pełne i ciężkie, pędy boczne gotowe do pożerania.</p>
+    <p>Gdyby na świecie pozostała choć jedna para widzących oczu, mogłaby znaleźć pewne piękno w jej obojętności, nawet elegancję, patrząc, jak porusza się po pokoju. Drepcze wokół stłuczonych kieliszków do wina. Zgarnia porozrzucane scrabble. Wspina się na plazmowy telewizor. Nadaje sofie nowy, liściasty look. Czy też wypycha z głowy lalki plastikowe oko.</p>
+    <p>W żywą plątaninę swojego rozrastającego się ciała ze spokojem włącza grube skarpety, butelki drogich perfum, kryminały, fikuśną bieliznę, suche świerkowe igły, poradniki, drewniane figurki zwierząt gospodarskich, elektroniczne gadżety, strzępy ozdobnego papieru, biżuterię, opróżnione blistry. Mimo swej wszechobecności nie widzi jednak różnicy między uschłą choinką a dyndającymi kośćmi.</p>
+    <p>Powoli, lecz systematycznie pochłania pozostałe pokoje: mroczną, wilgotną łazienkę, przestronną kuchnię z widokiem na zieloną autostradę, dwie ciche sypialnie. Wydaje się nieuniknione, że jej turgor zatrzyma meble w wiecznym uwięzieniu. Że pokoje dostaną nowe imiona na cześć jej wakuol. Gdyby na świecie pozostało choć jedno bijące serce, mogłoby znaleźć pocieszenie w myśli, że w doniczkach, pustym akwarium i maleńkiej klatce piersiowej jeszcze raz zagości życie.</p>
 
 </div>;
