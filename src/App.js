5e17d89b6ce52fc8d8e7109906bc7369da205c90
Łukasz Drobnik <drobniklukasz@gmail.com>
2019-07-10 19:02:37 +0200

    add complementary colours to styled-components theme

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ import Home from './components/Home/Home';
 const theme = {
     //colours
     nocturine: '#a98086',
+    nocturine2: '#ca8ca3',
     cunninghamella: '#566f0c',
+    cunninghamella2: '#85a814',
     vostok: '#a67847',
+    vostok2: '#c69f25',
     entropia: '#616a65',
+    entropia2: '#228671',
     devonian: '#99aac6',
+    devonian2: '#3fc6eb',
     obrovsky: '#b34a53',
+    obrovsky2: '#e44f60',
     lightColor: '#e5e5e5',
     darkColor: '#1b1b1b',
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
@@ -12,7 +13,9 @@ const store = createStore(reducer);
 
 const app = (
   <Provider store={store}>
-      <App />
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
   </Provider>
 );
 
