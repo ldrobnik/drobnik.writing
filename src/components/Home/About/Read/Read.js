9aa9ae14ed2495b8ec914974dfe8b46032a321b5
Łukasz Drobnik <drobniklukasz@gmail.com>
2019-07-11 10:44:56 +0200

    add router links to Read section

--- src/components/Home/About/Read/Read.js
+++ src/components/Home/About/Read/Read.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
+import { Link } from "react-router-dom";
 
 const StyledWrapper = styled.div`
 
@@ -9,7 +10,13 @@ const Read = (props) => {
 
     return (
         <StyledWrapper>
-            Read
+            <h2>Read some of my work</h2>
+            <p><Link to='/texts/nocturine'>Nocturine</Link></p>
+            <p><Link to='/texts/cetacean'>Cetacean</Link></p>
+            <p><Link to='/texts/moths'>Moths</Link></p>
+            <p><Link to='/texts/cellulose'>Cellulose</Link></p>
+            <p><Link to='/texts/treasures'>Treasures</Link></p>
+            <p><Link to='/texts/landmines'>Landmines</Link></p>
         </StyledWrapper>
     );
 };
