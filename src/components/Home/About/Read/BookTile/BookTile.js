529d0065515ca9681f2b99d341d11cfce3485f84
Łukasz Drobnik <drobniklukasz@gmail.com>
2019-07-11 10:48:40 +0200

    add BookTile, CollectionTile and PressTile components

---
+++ src/components/Home/About/Read/BookTile/BookTile.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react';
+import styled from 'styled-components';
+
+const StyledWrapper = styled.div`
+
+`;
+
+const BookTile = (props) => {
+
+    return (
+        <StyledWrapper>
+
+        </StyledWrapper>
+    );
+};
+
+export default BookTile;
---
+++ src/components/Home/About/Read/PressTile/PressTile.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react';
+import styled from 'styled-components';
+
+const StyledWrapper = styled.div`
+
+`;
+
+const PressTile = (props) => {
+
+    return (
+        <StyledWrapper>
+
+        </StyledWrapper>
+    );
+};
+
+export default PressTile;
