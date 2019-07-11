a88bbfd45d54edc33a2e1d652eb4bf23effef8c3
Łukasz Drobnik <drobniklukasz@gmail.com>
2019-07-11 18:15:15 +0200

    add ScrollableAnchors to About component

--- src/components/Home/About/About.js
+++ src/components/Home/About/About.js
@@ -1,13 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
+import ScrollableAnchor from 'react-scrollable-anchor'
 
 import Intro from './Intro/Intro';
 import Nocturine from './Nocturine/Nocturine';
 import Pubs from './Pubs/Pubs';
 import Read from './Read/Read';
 import {TEXTS} from "../../../data/constants";
-
 
 
 const StyledWrapper = styled.div`
@@ -32,10 +32,16 @@ const About = (props) => {
 
     return (
         <StyledWrapper>
-            <Intro />
-            <Nocturine />
-            <Pubs />
-            <Read />
+            <Intro/>
+            <ScrollableAnchor id={'nocturine'}>
+                <Nocturine/>
+            </ScrollableAnchor>
+            <ScrollableAnchor id={'pubs'}>
+                <Pubs/>
+            </ScrollableAnchor>
+            <ScrollableAnchor id={'read'}>
+                <Read/>
+            </ScrollableAnchor>
 
         </StyledWrapper>
     );
