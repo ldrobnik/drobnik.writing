b3c3a2fb20e3f4f8c6c2564af2021fb42ad87d4d
Łukasz Drobnik <drobniklukasz@gmail.com>
2019-07-15 14:19:43 +0200

    improve the display of social media links in Intro

--- src/components/Home/About/Intro/Intro.js
+++ src/components/Home/About/Intro/Intro.js
@@ -22,20 +22,17 @@ const Intro = (props) => {
                 )
                 }
             )}
-            {WEBSITE_TEXT.intro.patreon[props.lang]}
             <p>
                 {WEBSITE_TEXT.intro.social.map((link, k) => {
                     //don't display the separator after the last link
                     separator = (k < (WEBSITE_TEXT.intro.social.length - 1)) ? ' | ' : '';
-                    console.log(WEBSITE_TEXT.intro.social.length, k);
                     return (
-                            <p key={k}>
-                                <a href={link.url} target="_blank" rel="noopener noreferrer">{link.name}</a>{separator}
-                            </p>
+                                <a key={k}href={link.url} target="_blank" rel="noopener noreferrer">{link.name}{separator}</a>
                         )
                     }
                 )}
             </p>
+            {WEBSITE_TEXT.intro.patreon[props.lang]}
             <p>Consectetur adipisicing elit. Architecto dignissimos ducimus est nesciunt quas vero. Aliquid amet aspernatur at blanditiis cupiditate dignissimos ducimus ea eaque eius excepturi facere facilis ipsum iure iusto labore maiores minus molestias nobis non odio, odit perferendis perspiciatis porro possimus, quae qui quia quibusdam quis rerum sapiente sed sint, suscipit tempore unde voluptate voluptatem voluptatibus? Doloremque ducimus ea natus.</p>
             <p>Dolor sit amet, consectetur adipisicing elit. Ab aperiam aut dolores ea, eum explicabo fuga ipsum iusto mollitia nemo odit pariatur porro quam quasi recusandae rem repellendus, sed totam velit voluptatum?</p>
             <p><AnchorLink href='#publications'>Publications</AnchorLink></p>
