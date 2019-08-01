652d8baff1d6fd2e5c802806dfabbdbcec6e66bf
Łukasz Drobnik <drobniklukasz@gmail.com>
2019-08-01 19:25:26 +0200

    display text description in the Text component

--- src/components/Home/Text/Text.js
+++ src/components/Home/Text/Text.js
@@ -145,6 +145,7 @@ const Text = (props) => {
             <TextBody>
                 {TEXTS[props.lang][textName].content}
                 <p><i>{TEXTS[props.lang][textName].credits}</i></p>
+                <p><i>{TEXTS[props.lang][textName].description}</i></p>
                 <p>
                     <i>{nextTextLink}</i>&nbsp;|&nbsp;
                     <i>{backToTop}</i>&nbsp;|&nbsp;
