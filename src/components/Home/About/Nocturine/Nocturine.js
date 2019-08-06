5efb41c875b93b31a2a5aef85b893bd481434e56
Łukasz Drobnik <drobniklukasz@gmail.com>
2019-08-06 14:50:52 +0200

    add formatting to the book title in Nocturine section

--- src/components/Home/About/Nocturine/Nocturine.js
+++ src/components/Home/About/Nocturine/Nocturine.js
@@ -23,6 +23,13 @@ const Body = styled.div`
     a {
       font-family: ${props => props.theme.sansSerif};
     }
+    
+    .bookTitle {
+    text-transform: uppercase;
+    font-weight: bold;
+    font-style: italic;
+    font-family: ${props => props.theme.sansSerif};
+    }
 `;
 
 /* POSE */
