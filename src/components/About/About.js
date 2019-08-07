391b951ab66eca02ba89dd9be65a5eb033e7769d
Łukasz Drobnik <drobniklukasz@gmail.com>
2019-08-07 09:53:53 +0200

    remove the scroll to top code - reduntant after adding spinner

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -84,11 +84,6 @@ const About = (props) => {
         props.onSetTextPage(false)
     };
 
-    //scroll to top on page reload
-    useEffect(() => {
-        window.scrollTo(0,0);
-    }, []);
-
     useEffect(() => {
         //Update page title with the piece title
         document.title = `Łukasz Drobnik - ${fictionWriter}`;
