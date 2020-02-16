e3a676781b205e7b6a92bc594604ad8c242d84d7
Łukasz Drobnik <drobniklukasz@gmail.com>
2020-02-16 18:05:27 +0100

    configure enzyme adapter

---
+++ src/components/UI/CentredPhoto/CenteredPhoto.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import CenteredPhoto from './CentredPhoto';
+
+describe('CenteredPhoto', () => {
+    let centeredPhoto = mount(<CenteredPhoto />);
+});
