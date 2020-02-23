a9e283c280ad34dd64fd0e2a2ab3aecc374fb7eb
Łukasz Drobnik <drobniklukasz@gmail.com>
2020-02-23 22:25:06 +0100

    turn off google analytics test mode

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import Home from './components/Home/Home';
 
 //Google Analytics code
 const ANALYTICS_KEY = process.env.REACT_APP_ANALYTICS_KEY; // Google analytics API key
-ReactGA.initialize(ANALYTICS_KEY, { testMode: true });
+ReactGA.initialize(ANALYTICS_KEY);
+// ReactGA.initialize(ANALYTICS_KEY, { testMode: true });
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 
