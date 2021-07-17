ab7121e1f023c0c7c701eb4d08c14c534332a45a
Łukasz Drobnik <drobniklukasz@gmail.com>
2021-07-17 10:13:14 +0200

    move styled-components code from CentredButton.js

--- src/styled.js
+++ src/styled.js
@@ -640,3 +640,46 @@ export const NextTextLine = styled.div`
   background-color: ${props => props.theme.darkColor};
 `;
 
+/* CentredButton.js */
+
+export const CentredButtonOuterWrapper = styled.div`
+  text-align: center;
+  margin: 1em 0;
+`;
+
+export const CentredButtonButtonWrapper = styled.div`
+  background-color: ${props => props.theme.background};
+  font-size: ${props => props.theme.bodySize};
+  font-weight: bold;
+  padding: 0.8em;
+  margin: 0 0.5em;
+  cursor: pointer;
+  display: block;
+  position: relative;
+  user-select: none;
+  overflow: hidden;
+        
+  &:hover {
+      animation: ${POP_KEYFRAMES} ${props => props.theme.popAnimation}
+  }
+    
+  @media all and (min-width: ${props => props.theme.extraSmallScr}) {
+       margin: 0 0.8em;;
+  }
+    
+  @media all and (min-width: ${props => props.theme.smallScr}) {
+       margin: 0 4em;
+  }
+    
+  @media all and (min-width: ${props => props.theme.mediumScr}) {
+      margin: 0 2em;
+  }
+   
+  @media all and (min-width: ${props => props.theme.largeScr}) {
+       margin: 0 20%;
+  }
+    
+  @media all and (min-width: ${props => props.theme.extraLargeScr}) {
+       margin: 0 25%;
+  }  
+`;
