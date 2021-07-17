12f9efe4198716bcf19c57b761be8910580ff8b8
Łukasz Drobnik <drobniklukasz@gmail.com>
2021-07-17 10:04:02 +0200

    move styled-components code from DescriptionPanel.js

--- src/styled.js
+++ src/styled.js
@@ -592,3 +592,23 @@ export const CreditWrapper = styled.div`
   font-family: ${props => props.theme.serif};
   margin: 2em 0 2.5em 0;
 `;
+
+/* DescriptionPanel.js */
+
+export const Description = styled.div`
+  background-color: ${props => props.theme.background};
+  font-size: ${props => props.theme.bodySize};
+  text-align: center;
+  padding: 0.5em 1em;
+  margin: 2.5em 0;
+  
+  a {
+    font-weight: bold;
+    
+    @media all and (min-width: ${props => props.theme.smallScr}) {
+             &:hover {
+                      animation: ${PULSATE_KEYFRAMES} ${props => props.theme.pulsateAnimation};
+             }
+        }  
+  }
+`;
