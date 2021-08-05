3972572674c6b291a3080cace6c1c873c35d9874
Łukasz Drobnik <drobniklukasz@gmail.com>
2021-08-05 21:11:55 +0200

    create BlogNote.js

---
+++ src/components/Blog/BlogNote/BlogNote.js
@@ -0,0 +1,151 @@
+import React, {useState, useEffect} from 'react';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import Markdown from 'markdown-to-jsx/dist/index.js';
+import {
+    setTheme,
+    setCurrentText,
+    setNavbarVisibility,
+    setDataNoticeVisible,
+    setPage,
+    setPageReload
+} from '../../../actions';
+import {AnimatedContent} from '../../../posed';
+import {BlogTopAnchor, BlogNote, BlogWrapper, FADE_DURATION} from '../../../styled';
+import {TEXT_NAMES, TEXTS} from './../../../data/constants';
+import ThemeWrapper from './../ThemeWrapper/ThemeWrapper';
+import SectionSeparator from '../../UI/SectionSeparator/SectionSeparator';
+import InvisibleSeparator from '../../UI/InvisibleSeparator/InvisibleSeparator';
+import SubpageLinks from '../../UI/SubpageLinks/SubpageLinks';
+import CopyrightNote from '../../UI/CopyrightNote/CopyrightNote';
+
+export const Blog = props => {
+
+    //filepath to a sample markdown document
+    const filename = 'markdown';
+
+    //blogpost to be displayed
+    const [post, setPost] = useState('');
+
+    //shows the content
+    const showContent = () => {
+        props.setPageReload(false);
+    };
+
+    //sets off page reloading animation
+    const reloadPage = () => {
+        props.setPageReload(true);
+    };
+
+    //checks whether the data storage notice should be displayed and turns it on if it is invisible but hasn't been confirmed yet
+    const checkDataNotice = () => {
+        if (!props.noticeAccepted) {
+            props.setDataNoticeVisible(true);
+        }
+    };
+
+    //shows the NavBar
+    const showNavbar = () => {
+        props.setNavbarVisibility(true);
+    };
+
+    //sets black and white theme
+    const setBwTheme = () => {
+        props.setTheme('blackAndWhite');
+    };
+
+    //lets the Redux store know which page is currently displayed
+    const setCurrentPage = (page) => {
+        props.setPage(page);
+    }
+
+    useEffect(() => {
+        //Update page title with the piece title
+        document.title = 'Łukasz Drobnik - Blog';
+
+        //sets theme to black and white
+        setBwTheme();
+
+        //show Navbar
+        showNavbar();
+
+        //checks whether data storage notice should be visible and if so, turn is on
+        checkDataNotice();
+
+        //lets the Redux store know that the Text page is currently displayed
+        setCurrentPage('blog');
+
+        //show content after a while if page has loaded
+        if (props.loaded) {
+            setTimeout(showContent, FADE_DURATION);
+        }
+
+    });
+
+    useEffect(() => {
+        //imports markdown documents and coverts it into text
+        import(`./../../../data/blognotes/${filename}.md`)
+            .then(res => {
+                fetch(res.default)
+                    .then(res => res.text())
+                    .then(res => setPost(res))
+                    .catch(err => console.log(err));
+            })
+            .catch(err => console.log(err));
+
+    });
+
+    //do not show the content until the page is loaded
+    return props.loaded &&
+        <BlogWrapper>
+            <BlogTopAnchor>
+                <div id='top'></div>
+            </BlogTopAnchor>
+            <AnimatedContent
+                pose={!props.reload ? 'visible' : 'hidden'}>
+                <ThemeWrapper theme={'tech'}>
+                    <BlogNote>
+                        <div>hej!</div>
+                        <Markdown
+                            options={{
+                                overrides: {
+                                    a: {
+                                        props: {
+                                            target: '_blank',
+                                            rel: 'noopener noreferrer'
+                                        },
+                                    },
+                                },
+                            }}
+                        >
+                            {post}
+                        </Markdown>
+                    </BlogNote>
+                </ThemeWrapper>
+            </AnimatedContent>
+        </BlogWrapper>;
+};
+
+const mapStateToProps = state => {
+    return {
+        lang: state.language,
+        bwMode: state.blackAndWhite,
+        noticeVisible: state.dataNoticeVisible,
+        noticeAccepted: state.dataNoticeAccepted,
+        reload: state.pageReload,
+        loaded: state.pageLoaded
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return bindActionCreators({
+        setTheme,
+        setCurrentText,
+        setNavbarVisibility,
+        setDataNoticeVisible,
+        setPage,
+        setPageReload
+    }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Blog);
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,6 +9,7 @@ import Text from '../Text/Text';
 import BookPage from '../BookPage/BookPage';
 import QuickLinks from '../QuickLinks/QuickLinks';
 import Blog from '../Blog/Blog';
+import BlogNote from '../Blog/BlogNote/BlogNote';
 import DataNotice from '../UI/DataNotice/DataNotice';
 import Spinner from '../UI/Spinner/Spinner';
 import {GlobalStyle} from '../../styled';
@@ -91,7 +92,7 @@ export const Home = (props) => {
                     <Route path="/vostok/" exact component={() => <BookPage book={1}/>} key="vostok"/>
                     <Route path="/links/" exact component={QuickLinks} key="links"/>
                     <Route path="/blog/" exact component={Blog} key="blog"/>
-                    <Route path="/blog/:id" exact component={Blog} key="blognote"/>
+                    <Route path="/blog/:id" exact component={BlogNote} key="blognote"/>
                     <Route render={() => (<Redirect to="/"/>)} key="default"/>
                 </Switch>
                 <DataNotice/>
