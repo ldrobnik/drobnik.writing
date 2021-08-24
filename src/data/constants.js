7ce8d164096ac1a404539db34e907a96452f4059
Łukasz Drobnik <drobniklukasz@gmail.com>
2021-08-24 17:17:15 +0200

    add alt texts to blog top images

--- src/data/constants.js
+++ src/data/constants.js
@@ -1410,7 +1410,7 @@ export const BLOG_NOTES = [
         imageCredits: {
             author: '',
             source: '',
-            alt: ''
+            alt: 'A text editor with the beginning of Joyce’s Ulysses written in Markdown'
         }
 
     },
@@ -1423,7 +1423,7 @@ export const BLOG_NOTES = [
         imageCredits: {
             author: '',
             source: '',
-            alt: ''
+            alt: 'A text editor showing basics Markdown syntax, such as headings and lists, and its preview'
         }
 
     },
