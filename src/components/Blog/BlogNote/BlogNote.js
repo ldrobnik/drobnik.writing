ae053f540d64ae0328e2552e7a6c10df9c12851e
Łukasz Drobnik <drobniklukasz@gmail.com>
2021-08-24 08:44:25 +0200

    safeguard against loading an empty blog note

--- src/components/Blog/BlogNote/BlogNote.js
+++ src/components/Blog/BlogNote/BlogNote.js
@@ -276,7 +276,7 @@ export const BlogNote = props => {
     }, [props.reload]);
 
     //do not show the content until the page is loaded
-    return props.loaded && (noteId.length > 0) &&
+    return props.loaded && (noteId.length > 20) &&
         <BlogWrapper>
             <BlogTopAnchor>
                 <div id='top'></div>
